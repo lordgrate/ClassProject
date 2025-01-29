1e94fa2889f5bc537dec21b4ba4ea5058af54cd7
KeiliaX <ericaloia1111@gmail.com>
2025-01-29 16:39:48 -0500

    changes

---
+++ Assets/Scripts/Modifiers/HealingModifier.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingModifier : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private bool ishealed = false;
+    [SerializeField]
+    private int _HealingAMT = 0;
+    [SerializeField]
+    private GameObject _Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
